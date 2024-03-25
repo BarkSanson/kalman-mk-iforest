902a43fbcb1f6c4c23e04cac4503cd999366822c
Bark Sanson <barksansondev@gmail.com>
2024-03-25 10:36:42 +0100

    Initial commit

--- main.py
+++ main.py
@@ -6,47 +6,50 @@ from mkiforest import MKKalmanIForestPipeline
 
 DATA = [
     'SunburyLock_2019-05-01_2019-07-15.csv',
-    'SunburyLock_2024-02-01_2024-02-29.csv'
+    'SunburyLock_2024-02-01_2024-02-29.csv',
+    'SunburyLock_2019-01-01_2024-02-01.csv'
 ]
+
+SLIDING = False
 
 
 def main():
-    window_size = 64
+    window_size = 128
     slope_threshold = 0.001
 
-    df = pd.read_csv(f'data/{DATA[0]}')
+    df = pd.read_csv(f'data/{DATA[1]}')
 
     df = df.set_index(pd.to_datetime(df['dateTime']))
 
-    df = df.drop(columns=['quality', 'dateTime'])
-    #df = df.drop(columns=['quality', 'dateTime', 'completeness', 'qcode', 'date', 'measure'])
+    #df = df.drop(columns=['quality', 'dateTime'])
+    df = df.drop(columns=['quality', 'dateTime', 'completeness', 'qcode', 'date', 'measure'])
 
-    z_score = (df['value'] - df['value'].mean()) / df['value'].std()
-    df[abs(z_score) > 3] = df['value'].mean()
+    #z_score = (df['value'] - df['value'].mean()) / df['value'].std()
+    #df[abs(z_score) > 3] = df['value'].mean()
 
-    flood = np.random.normal(0, 0.05, 40) + np.linspace(df['value'].iloc[100], 1, 40)
-    for i in range(100, 141):
-        df.iloc[i] = flood[i - 101]
+    #flood = np.random.normal(0, 0.05, 40) + np.linspace(df['value'].iloc[100], 1, 40)
+    #for i in range(100, 141):
+    #    df.iloc[i] = flood[i - 101]
 
-    flood = 1 + np.random.normal(0, 0.05, 59)
-    for i in range(141, 199):
-        df.iloc[i] = flood[i - 140]
+    #flood = 1 + np.random.normal(0, 0.05, 59)
+    #for i in range(141, 199):
+    #    df.iloc[i] = flood[i - 140]
 
-    flood = np.random.normal(0, 0.05, 40) + np.linspace(df['value'].iloc[199], 0, 40)
-    for i in range(199, 239):
-        df.iloc[i] = flood[i - 199]
+    #flood = np.random.normal(0, 0.05, 40) + np.linspace(df['value'].iloc[199], 0, 40)
+    #for i in range(199, 239):
+    #    df.iloc[i] = flood[i - 199]
 
-    flood = np.random.normal(0, 0.05, 60) + np.linspace(df['value'].iloc[100], 1, 60)
-    for i in range(4000, 4061):
-        df.iloc[i] = flood[i - 4001]
+    #flood = np.random.normal(0, 0.05, 60) + np.linspace(df['value'].iloc[2000], 1, 60)
+    #for i in range(2000, 2061):
+    #    df.iloc[i] = flood[i - 2001]
 
-    flood = 1 + np.random.normal(0, 0.05, 100)
-    for i in range(4061, 4161):
-        df.iloc[i] = flood[i - 4061]
+    #flood = 1 + np.random.normal(0, 0.05, 100)
+    #for i in range(2061, 2161):
+    #    df.iloc[i] = flood[i - 2061]
 
-    flood = np.random.normal(0, 0.05, 70) + np.linspace(df['value'].iloc[199], 0, 70)
-    for i in range(4161, 4231):
-        df.iloc[i] = flood[i - 4161]
+    #flood = np.random.normal(0, 0.05, 70) + np.linspace(df['value'].iloc[2161], 0, 70)
+    #for i in range(2161, 2231):
+    #    df.iloc[i] = flood[i - 2161]
 
     mkif = MKKalmanIForestPipeline(window_size=window_size, slope_threshold=slope_threshold)
 
@@ -60,16 +63,15 @@ def main():
 
     df = df[:len(res)]
 
-    plt.figure()
     plt.plot(df.index, df['value'], label="Nivell de l'aigua")
-    plt.scatter(df.index, df['value'], c=res, cmap="seismic", s=1)
+    plt.scatter(df.index, df['value'], c=res, cmap="seismic", s=0.5)
     plt.colorbar()
-    plt.title(f'MKiForest with {window_size} window size, {slope_threshold} slope threshold, sliding window')
+    plt.title(f'MKiForest with {window_size} window size, {slope_threshold} slope threshold,'
+              f'{"sliding window" if SLIDING else "batch window"}.png')
 
+    plt.savefig(f'MKiForest with {window_size} window size, {slope_threshold} slope threshold,'
+                f'{"sliding window" if SLIDING else "batch window"}.png')
     plt.show()
-    plt.savefig(f'MKiForest with {window_size} window size, {slope_threshold} slope threshold, sliding window.png')
-
-    print()
 
 
 if __name__ == '__main__':
--- mkiforest.py
+++ mkiforest.py
@@ -1,19 +1,17 @@
-import collections
-
 import numpy as np
 from filterpy.kalman import KalmanFilter
-from sklearn.ensemble import IsolationForest
 from pymannkendall import yue_wang_modification_test
 from scipy.stats import wilcoxon
+from sklearn.ensemble import IsolationForest
+from river.drift import KSWIN
 
 
 class MKKalmanIForestPipeline:
     def __init__(self,
                  alpha: float = 0.05,
-                 contamination: float = "auto",
                  slope_threshold: float = 0.001,
                  window_size: int = 64):
-        self.model = IsolationForest(contamination=contamination)
+        self.model = IsolationForest()
         self.kf = KalmanFilter(dim_x=1, dim_z=1)
         self.kf.Q = 0.001
         self.kf.F = np.array([[1]])
@@ -25,8 +23,8 @@ class MKKalmanIForestPipeline:
         self.slope_threshold = slope_threshold
         self.window_size = window_size
 
-        self.raw_sliding_window = collections.deque(maxlen=window_size)
-        self.filtered_sliding_window = collections.deque(maxlen=window_size)
+        self.raw_window = np.array([])
+        self.filtered_window = np.array([])
 
         self.reference_window = np.array([])
         self.filtered_reference_window = np.array([])
@@ -36,7 +34,7 @@ class MKKalmanIForestPipeline:
         self.retrains = 0
 
     def update(self, x):
-        self.raw_sliding_window.append(x)
+        self.raw_window = np.append(self.raw_window, x)
 
         # Apply Kalman filter to current data
         self.kf.predict()
@@ -44,38 +42,50 @@ class MKKalmanIForestPipeline:
 
         filtered_x = self.kf.x
 
-        self.filtered_sliding_window.append(filtered_x)
+        self.filtered_window = np.append(self.filtered_window, filtered_x)
 
-        if len(self.raw_sliding_window) < self.window_size:
+        if len(self.raw_window) < self.window_size:
             return None
 
         if not self.warm:
-            self.reference_window = np.array(self.raw_sliding_window)
-            self.filtered_reference_window = np.array(self.filtered_sliding_window)
+            self.reference_window = self.raw_window.copy()
+            self.filtered_reference_window = self.filtered_window.copy()
 
-            self.model.fit(self.reference_window.reshape(-1, 1))
+            ref = self.reference_window.reshape(-1, 1)
+            self.model.fit(ref)
 
-            scores = self.model.score_samples(self.reference_window.reshape(-1, 1))
+            scores = self.model.score_samples(ref)
 
             self.warm = True
+            self.raw_window = np.array([])
+            self.filtered_window = np.array([])
 
             return scores
 
         _, h, _, _, _, _, _, slope, _ = \
-            yue_wang_modification_test(self.filtered_sliding_window)
-        #stat, p_value = wilcoxon(self.filtered_reference_window, self.filtered_sliding_window)
+            yue_wang_modification_test(self.filtered_window)
+        d = np.around(self.raw_window - self.reference_window, decimals=3)
+        stat, p_value = wilcoxon(d)
 
         # If the water level is rising or decreasing significantly, or the data is significantly different from the
         # reference, retrain the model
-        if h and abs(slope) >= self.slope_threshold:# or (p_value < self.alpha and stat > 0.1):
-            self._retrain()
+        if (h and abs(slope) >= self.slope_threshold) or p_value < self.alpha:
+            scores = self._retrain()
+            self.raw_window = np.array([])
+            self.filtered_window = np.array([])
+            return scores
 
-        score = self.model.score_samples(x.reshape(1, -1))
-        return score
+        scores = self.model.score_samples(self.raw_window.reshape(-1, 1))
+
+        self.raw_window = np.array([])
+        self.filtered_window = np.array([])
+        return scores
 
     def _retrain(self):
-        self.reference_window = np.array(self.raw_sliding_window)
-        self.filtered_reference_window = np.array(self.filtered_sliding_window)
+        self.reference_window = self.raw_window.copy()
+        self.filtered_reference_window = self.filtered_window.copy()
         self.model.fit(self.reference_window.reshape(-1, 1))
         self.retrains += 1
         print(f"Retraining model... Number of retrains: {self.retrains}")
+
+        return self.model.score_samples(self.reference_window.reshape(-1, 1))
