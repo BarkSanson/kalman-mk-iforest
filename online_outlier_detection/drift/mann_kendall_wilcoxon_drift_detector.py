4cc50428c0d497923828371193f3d0b229812484
Bark Sanson <barksansondev@gmail.com>
2024-05-14 13:11:13 +0200

    Fix

--- online_outlier_detection/drift/mann_kendall_wilcoxon_drift_detector.py
+++ online_outlier_detection/drift/mann_kendall_wilcoxon_drift_detector.py
@@ -6,7 +6,7 @@ from .base_drift_detector import BaseDriftDetector
 
 
 class MannKendallWilcoxonDriftDetector(BaseDriftDetector):
-    def __init__(self, alpha: float, slope_threshold: float, ):
+    def __init__(self, alpha: float, slope_threshold: float):
         self.alpha = alpha
         self.slope_threshold = slope_threshold
 
@@ -14,6 +14,6 @@ class MannKendallWilcoxonDriftDetector(BaseDriftDetector):
         _, h, _, _, _, _, _, slope, _ = \
             yue_wang_modification_test(x)
         d = np.around(x - y, decimals=3)
-        stat, p_value = wilcoxon(d)
+        stat, p_value = wilcoxon(d, zero_method='zsplit')
 
         return (h and slope > self.slope_threshold) or p_value < self.alpha
